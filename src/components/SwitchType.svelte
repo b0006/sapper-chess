45bad53da1648cff24607a6663add1c4aa9bb4a6
Kasikov <qsefthuken@gmail.com>
2019-11-27 17:07:29 +0700

    init commit

---
+++ src/components/SwitchType.svelte
@@ -0,0 +1,12 @@
+<style>
+	.switch {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
+
+<div class="switch">
+    <a href="/ai">Против ИИ</a>
+    <a href="/player">Против человека</a>
+</div>
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Sapper chess</title>
+</svelte:head>
+
+<script>
+	import SwitchType from '../components/SwitchType.svelte';
+</script>
+
+<SwitchType />
+
